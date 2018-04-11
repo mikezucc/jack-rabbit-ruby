5be8ce8b159110682ba508219914504d1cc7e2fd
Osiris maximA <mikezuccarino@gmail.com>
2018-04-11 05:08:43 -0700

    Fixed some comments and stuff

--- jack-listen.rb
+++ jack-listen.rb
@@ -8,10 +8,6 @@ require 'aws-sdk-dynamodb'
 
 # Good Ruby DynamoDB resource: https://gist.github.com/ujuettner/3914147
 
-## NOTE: It seems that AWS prefers to use and only use a combination of ~/.aws/credentials and
-## NOTE: environment variables with the credentials for access key, secret, AND region
-## NOTE: trying to override using aws-sdk ruby seems to improperly override the params
-## NOTE: causing a signature failure response from AWS 
 # 3rd party service configurations, aside from in memory/environment auth credentials
 # ensure that keys are stored in ~/.aws/credentials as
 # [default]
@@ -19,39 +15,43 @@ require 'aws-sdk-dynamodb'
 # aws_secret_access_key = <>
 # $DYNAMO_PUBLIC_KEY = ""
 # $DYNAMO_PRIVATE_KEY = ""
-# module Visibility_DataStore
-#   module Visibility_DynamoDB
-#     US_WEST_1 = "us-west-1"
-#     #
-#     # CONFIG Initialize the Aws Config environment
-#     # Set up the AWS environment based on a config file
-#     #
-#     def self.Configure(credentialsDir = File.expand_path("..", Dir.pwd), keyFile = 'dynamoboysprivatekey1.txt')
-#       keys_file = File.join(credentialsDir, keyFile)
-#       puts "[Visibility_DataStore::Visibility_DynamoDB] Looking for AWS credentials: " + keys_file
-#       unless File.exist?(keys_file)
-#         puts "#{keys_file} dne"
-#         exit 1
-#       end
-#
-#       line_number = 0
-#       File.open(keys_file, 'r') do |f|
-#         f.each_line do |line|
-#           case line_number
-#           when 0
-#             $DYNAMO_PRIVATE_KEY = line
-#           when 1
-#             $DYNAMO_PUBLIC_KEY = line
-#           end
-#           line_number += 1
-#         end
-#       end
-#     end
-#     #
-#     # CONFIG Initialize the Aws Config environment
-#     # Set up the AWS environment based on a config file
-#     #
-#   end
+module Visibility_DataStore
+  module Visibility_DynamoDB
+    US_WEST_1 = "us-west-1"
+    ## NOTE: It seems that AWS prefers to use and only use a combination of ~/.aws/credentials and
+    ## NOTE: environment variables with the credentials for access key, secret, AND region
+    ## NOTE: trying to override using aws-sdk ruby seems to improperly override the params
+    ## NOTE: causing a signature failure response from AWS
+    #     #
+    #     # CONFIG Initialize the Aws Config environment
+    #     # Set up the AWS environment based on a config file
+    #     #
+    #     def self.Configure(credentialsDir = File.expand_path("..", Dir.pwd), keyFile = 'dynamoboysprivatekey1.txt')
+    #       keys_file = File.join(credentialsDir, keyFile)
+    #       puts "[Visibility_DataStore::Visibility_DynamoDB] Looking for AWS credentials: " + keys_file
+    #       unless File.exist?(keys_file)
+    #         puts "#{keys_file} dne"
+    #         exit 1
+    #       end
+    #
+    #       line_number = 0
+    #       File.open(keys_file, 'r') do |f|
+    #         f.each_line do |line|
+    #           case line_number
+    #           when 0
+    #             $DYNAMO_PRIVATE_KEY = line
+    #           when 1
+    #             $DYNAMO_PUBLIC_KEY = line
+    #           end
+    #           line_number += 1
+    #         end
+    #       end
+    #     end
+    #     #
+    #     # CONFIG Initialize the Aws Config environment
+    #     # Set up the AWS environment based on a config file
+    #     #
+  end
 
   module Visibility_RabbitMQ
     ADDRESS_LOOPBACK = "localhost"
@@ -61,7 +61,7 @@ require 'aws-sdk-dynamodb'
 end
 
 
-Visibility_DataStore::Visibility_DynamoDB.Configure()
+# Visibility_DataStore::Visibility_DynamoDB.Configure()
 # AWS_credentials = Aws::Credentials.new($DYNAMO_PUBLIC_KEY, $DYNAMO_PRIVATE_KEY)
 $dynamodb = 0;
 class DynamoDB_Resource
